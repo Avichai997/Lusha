424247adccf980fe74556eecf3be0ccb1e19e6ca
Avichai iluz <avichai997@gmail.com>
2024-08-14 10:19:01 +0300

    update model name

--- scraping-system/src/components/url/url.model.ts
+++ scraping-system/src/components/url/url.model.ts
@@ -14,6 +14,6 @@ const urlSchema = new Schema<UrlType>(
   }
 );
 
-const ChatRoom = model<UrlType>('Url', urlSchema);
+const Url = model<UrlType>('Url', urlSchema);
 
-export default ChatRoom;
+export default Url;
